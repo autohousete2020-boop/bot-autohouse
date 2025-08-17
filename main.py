3121d008af1bd1615f99b76f0f3eeb2e4c5d735b
autohousete2020-boop <autohouse.te2020@gmail.com>
2025-08-17 14:09:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привіт! Це бот АвтоХаус 🚗")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
